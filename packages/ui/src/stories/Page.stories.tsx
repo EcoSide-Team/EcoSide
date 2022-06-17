33e6953fc70557be7911ab216e816dac37f16c24
Jakub Żojdzik <69974199+JakubZojdzik@users.noreply.github.com>
2022-06-17 20:59:07 +0200

    Setup monorepo (#8)
    * feat: Add `.vscode/` settings
    
    * feat: Setup Yarn Workspaces
    
    * feat: Add `.gitignore`
    
    * feat: Add ESLint config
    
    * feat: Add Prettier config
    
    * feat: Add Turborepo config
    
    * feat: Add `@ecoside/ui` package
    
    * feat: Add `@ecoside/database` package
    
    * feat: Add `@ecoside/dto` package
    
    * feat: Add `@ecoside/client` app
    
    * feat: Add `@ecoside/api` app
    
    * fix: Fix default formatters in `.vscode/` settings
    
    * feat(ui): Setup Storybook
    
    * feat: Setup imports sorting
    
    * chore: Format project
    
    * fix: Remove unnecessary imports
    
    * fix: Ignore Storybook static files
    
    * fix(ui): Fix Storybook typing issues
    
    * feat(api): Dockerize app
    
    * feat(client): Dockerize app
    
    * feat: Setup Docker Compose for development and production
    
    * fix: Add new patterns to `.dockerignore`
    
    * fix: Fix global `clean` script
    
    * fix(ui): Fix stories
    
    * feat(ui): Dockerize Storybook
    
    * feat: Add Storybook to current Docker Compose setup
    
    * fix: Setup tools behind nginx proxy
    
    /dev/pgadmin
    /dev/redis
    
    * fix(api): Fix import
    
    * fix(ui): Fix Storybook websockets passing to proxy
    
    * fix: Remove unused `.docker.env.example` file
    
    Co-authored-by: Maciej Opaliński <poprostumieciek@gmail.com>

---
+++ packages/ui/src/stories/Page.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+
+import { Page } from './Page';
+
+export default {
+    title: 'Example/Page',
+    component: Page,
+    parameters: {
+        // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'fullscreen',
+    },
+} as ComponentMeta<typeof Page>;
+
+const Template: ComponentStory<typeof Page> = () => <Page />;
+
+// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+export const LoggedOut = Template.bind({});
+
+export const LoggedIn = Template.bind({});
+LoggedIn.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const loginButton = canvas.getByRole('button', { name: /Log in/i });
+    await userEvent.click(loginButton);
+};
---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,24 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), checker({ typescript: true })],
+    build: {
+        outDir: 'build',
+        lib: {
+            name: '@ecoside/ui',
+            entry: './src/index.tsx',
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+    },
+});
