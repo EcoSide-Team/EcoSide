c521080e38d335199cf8f69aaccaa824a5ac0b15
Maciej Opaliński <poprostumieciek@gmail.com>
2022-06-20 00:27:12 +0200

    feat(api): Add `DatabaseModule` (#16)

---
+++ apps/api/src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { User } from '@ecoside/database';
+import { ConfigModule, ConfigService, NodeEnv } from '../config';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                ...configService.databaseCredentials(),
+                // logging: configService.NODE_ENV === NodeEnv.DEVELOPMENT,
+                synchronize: configService.NODE_ENV === NodeEnv.DEVELOPMENT,
+                entities: [User],
+            }),
+        }),
+    ],
+})
+export class DatabaseModule {}
