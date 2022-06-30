ce8190a4525a5b81cef1d2fa09f3f8217f7d565c
Maciej Opaliński <poprostumieciek@gmail.com>
2022-06-30 23:28:16 +0200

    feat(api): Initialize session and passport in `AppModule`

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Inject, MiddlewareConsumer, Module } from '@nestjs/common';
+import * as ConnectRedis from 'connect-redis';
+import * as session from 'express-session';
+import Redis from 'ioredis';
+import * as passport from 'passport';
 
+import { Time } from '@ecoside/utils';
 import { AuthModule } from '../auth';
-import { ConfigModule } from '../config';
+import { ConfigModule, ConfigService, NodeEnv } from '../config';
 import { DatabaseModule } from '../database';
 import { EmailModule } from '../email';
-import { RedisModule } from '../redis';
+import { REDIS, RedisModule } from '../redis';
 import { UserModule } from '../user';
 import { AppController, AppService } from '.';
+
+const RedisStore = ConnectRedis(session);
 
 @Module({
     imports: [
@@ -20,4 +27,33 @@ import { AppController, AppService } from '.';
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(
+        @Inject(REDIS) private redis_client: Redis,
+        private config: ConfigService,
+    ) {}
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(
+                session({
+                    secret: this.config.COOKIE_SECRET,
+                    resave: false,
+                    saveUninitialized: false,
+                    cookie: {
+                        maxAge: 7 * Time.DAY,
+                        httpOnly: true,
+                        secure: 'auto',
+                        sameSite:
+                            this.config.NODE_ENV === NodeEnv.DEVELOPMENT
+                                ? undefined
+                                : 'none',
+                    },
+                    store: new RedisStore({ client: this.redis_client }),
+                }),
+                passport.initialize(),
+                passport.session(),
+            )
+            .forRoutes('*');
+    }
+}
